86b72841c1ad2a1e4079e41a22f4eb3a8156fed8
imran <imranbabuji162002@gmail.com>
2024-08-31 11:53:43 +0530

    first commit

---
+++ data_types.java
@@ -0,0 +1,29 @@
+class data_types {
+    public static void main(String[] args) {
+
+        /// integer
+        int a = 20;
+        System.out.println(a);
+
+        /// float
+        float b = 30.4f;
+        System.out.println(b);
+
+        /// double
+        double c = 40;
+        System.out.println(c);
+
+        /// string
+        String d = "Imran";
+        System.out.println(d);
+
+        /// character
+        char e = 'e';
+        System.out.println(e);
+
+        /// boolean
+        boolean isRead = true;
+        System.out.println(isRead);
+
+    }
+}
---
+++ real_life_eg_data_types.java
@@ -0,0 +1,23 @@
+public class real_life_eg_data_types {
+
+  public static void main(String[] args) {
+
+    String studentName = "Imran B";
+    char studentStartingLetter = 'I';
+    int studentAge = 14;
+    int studentId = 44;
+    float studentFees = 55.000f;
+    double studentPassPercentage = 55.0;
+    boolean studentAllPass = true;
+
+    System.out.println(studentName);
+    System.out.println(studentStartingLetter);
+    System.out.println(studentAge);
+    System.out.println(studentId);
+    System.out.println(studentFees);
+    System.out.println(studentPassPercentage);
+    System.out.println(studentAllPass);
+
+  }
+
+}
