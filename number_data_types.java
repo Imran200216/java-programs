7727a0c3c5c7264e750978f014acc8190689531e
imran <imranbabuji162002@gmail.com>
2024-08-31 12:18:44 +0530

    number_data_types in java.

---
+++ number_data_types.java
@@ -0,0 +1,32 @@
+class number_data_types {
+
+    public static void main(String[] args) {
+
+        /*
+         * Types of data types in number
+         * 1. Byte - instead of int we can use byte it stores whole numbers -128 to 127
+         * 2. Short - it stores the whole numbers from -32768 to 32767
+         * 3. Int - it stores the whole numbers
+         * 4. Long
+         * 5. Float
+         * 6. Double
+         */
+
+        byte myBytes = 100;
+        System.out.println(myBytes);
+
+        short myShort = 5000;
+        System.out.println(myShort);
+
+        long myLong = 2000;
+        System.out.println(myLong);
+
+        float myFloat = 2.45f;
+        System.out.println(myFloat);
+
+        double myDouble = 2.00d;
+        System.out.println(myDouble);
+
+    }
+
+}
